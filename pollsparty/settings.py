e4867997e80780ab06b895c1000fddf51b625ee0
RubensRafael <rubensrafael2@live.com>
2021-08-04 16:26:26 -0300

    rest framework instalado corretamente

--- pollsparty/settings.py
+++ pollsparty/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pollspartyapp',
-    'rest_framework.authtoken',
     'rest_framework',
+    'rest_framework.authtoken',
+    
     
 ]
 
