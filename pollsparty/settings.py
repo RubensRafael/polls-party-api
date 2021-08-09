97cfbdfd6e305d73107d6fdd1db0d92865d1c452
RubensRafael <70234898+RubensRafael@users.noreply.github.com>
2021-08-09 15:01:26 -0300

    Adicionando allowed host

--- pollsparty/settings.py
+++ pollsparty/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["polls-party-api.herokuapp.com"]
 
 
 # Application definition
