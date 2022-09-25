5f6debce96d69e0a2b480c5492abf5df53bd6bf3
Rubens Rafael <70234898+RubensRafael@users.noreply.github.com>
2022-09-25 15:20:19 -0300

    Update settings.py

--- pollsparty/settings.py
+++ pollsparty/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost","polls-party-api.herokuapp.com", '127.0.0.1']
+ALLOWED_HOSTS = ["localhost","polls-party-api.herokuapp.com", "127.0.0.1", "polls-party-api.onrender.com"]
 
 
 # Application definition
